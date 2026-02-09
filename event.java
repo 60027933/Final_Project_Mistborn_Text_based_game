a4d65234e0d4041f252459ea9236246ede727ff8
60027933 <144839037+60027933@users.noreply.github.com>
2026-02-09 08:46:43 -0700

    Starting Psuedocode

---
+++ event.java
@@ -0,0 +1,12 @@
+
+
+public class event {
+    //so the event needs to hold this info
+    //introductory text options in an array ("book text")
+
+    //event types:
+    // enemy / inventory / information / quest (enum?)
+
+    // each event has requirements? like in inventory, 
+    // a requirement would be that you do not have the item in your inventory, depending on the item
+}
---
+++ textGame.java
@@ -0,0 +1,28 @@
+import java.util.*;
+// enum inventory objs;
+class textGame {
+    public static void main(String[] args) {
+        int year = 0;
+        int month = 0;
+        int day = 0;
+        //enum list inventory dynamic array?
+        // location / map here?
+        //each location needs a certain number of related 'filler' events
+    }
+
+    public static void gameTurn(){
+        //first: is there any immediate time based events that are happening right now? if so do that
+        //second: is there any time and location based "Special" events happening right now? if so do that, next turn
+        //third: is there any quests you are currently on? if so do that
+        //fourth: give options to player,
+        // travel, sleep, explore
+        // travel: select "travel event", travel to selected location
+        // sleep: gain health, stamina
+        // explore: local "filler" events
+    }
+    public static void clear(){
+        // clear the screen.
+    }
+}
+
+
