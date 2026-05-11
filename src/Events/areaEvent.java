7658a8893d289576afa7afc2edc9d9b2bc8271e1
60027933 <60027933@ironschools.org>
2026-05-11 08:48:26 -0600

    AreaEvent HealthOfEnemy function

--- src/Events/areaEvent.java
+++ src/Events/areaEvent.java
@@ -1,16 +1,21 @@
 package src.Events;
-enum event {
-    ENEMY,
-    INFO,
-    INVENTORY,
-    QUEST
-}
-enum enemies {
-    IMMATUREKOLOSS,
-    CLERGYSCOUT
-}
 
-class areaEvent {
+
+public class areaEvent {
+    public enum event {
+        ENEMY,
+        INFO,
+        INVENTORY,
+        QUEST
+    }
+    public enum enemies {
+        IMMATURE_KOLOSS,
+        CLERGY_SCOUT,
+        MATURE_KOLOSS,
+        TINEYE_CLERGY,
+        STEEL_INQUISITOR,
+        SKAA_BANDIT,
+    }
     //so the event needs to hold this info
     //introductory text options in an array ("book text")
 
@@ -24,5 +29,29 @@ class areaEvent {
         this.eventType = eventType;
         this.description = description;
         this.enemies = enemies;
+    }
+    public int healthOfEnemy(enemies enemy){
+        int health = 0;
+        switch(enemy){
+            case IMMATURE_KOLOSS:
+                health = 40;
+                break;
+            case MATURE_KOLOSS:
+                health = 150;
+                break;
+            case CLERGY_SCOUT:
+                health = 20;
+                break;
+            case TINEYE_CLERGY:
+                health = 50;
+                break;
+            case STEEL_INQUISITOR:
+                health = 250;
+                break;
+            case SKAA_BANDIT:
+                health = 20;
+                break;
+        }
+        return health;
     }
 }
