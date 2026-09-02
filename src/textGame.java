e16e00577173c3683205ebfc7042aa1486d029e0
Joseph Pack <avidatanything@gmail.com>
2026-09-02 12:37:40 -0600

    Final-ish Event type Info logic

--- src/textGame.java
+++ src/textGame.java
@@ -19,6 +19,7 @@ class textGame {
         public int currency;
         public int phase = 0;
         public boolean[] areasVisited = new boolean[areas.values().length];
+        public String lastEventDescription = "Null and Void Starter Description";
 
         public player(int health, calandar time, areas location, int dmg, int currency){
             this.health = health;
@@ -29,8 +30,15 @@ class textGame {
         }
         public randomAreaEvents pickEvent(){
             Random r = new Random();
-            eventTypes eventType = pickOption.fromEventTypes(new eventTypes[]{eventTypes.ENEMY,eventTypes.INFO},r);
-            randomAreaEvents returnValue = new randomAreaEvents(eventType, this.location);
+            randomAreaEvents returnValue = new randomAreaEvents(eventTypes.ENEMY, this.location); // default value
+            for(int count = 0; count < 100; count++){ // number of iterations trying to get a unique previous event 
+                eventTypes eventType = pickOption.fromEventTypes(new eventTypes[]{eventTypes.ENEMY,eventTypes.INFO},r);
+                returnValue = new randomAreaEvents(eventType, this.location);
+                if(!returnValue.description.equals(lastEventDescription)){
+                    lastEventDescription = returnValue.description;
+                    break;
+                }
+            }
             return returnValue;
         }
         
@@ -66,37 +74,37 @@ class textGame {
     public static boolean checkPlayerUnderstanding(Scanner s){
         boolean returnValue = true;
         
-        printAddons.printS("Welcome to the MISTBORN ADVENTURE GAME.\n\n");
+        pAdd.printS("Welcome to the MISTBORN ADVENTURE GAME.\n\n");
         boolean understanding = option(s, "Have you read mistborn?", "Yes","No");
         if(!understanding){
             boolean spoilers = option(s, "Are you okay with spoilers to Mistborn?","Yes", "No");
-            if(!spoilers){ printAddons.printS("\nGo read Mistborn. It's a fantastic book. This game can wait fr"); returnValue = false;}
+            if(!spoilers){ pAdd.printS("\nGo read Mistborn. It's a fantastic book. This game can wait fr"); returnValue = false;}
             if(!understanding && spoilers) {
-                printAddons.printS("Okay so Mistborn is a series of epic fantasy novels by the author Brandon Sanderson and published by Tor Books. The first trilogy, commonly referred to as \"Era One\", was published between 2006 and 2008 and consists of The Final Empire, The Well of Ascension, and The Hero of Ages.");
-                printAddons.pause();printAddons.pause();
+                pAdd.printS("Okay so Mistborn is a series of epic fantasy novels by the author Brandon Sanderson and published by Tor Books. The first trilogy, commonly referred to as \"Era One\", was published between 2006 and 2008 and consists of The Final Empire, The Well of Ascension, and The Hero of Ages.");
+                pAdd.pause();pAdd.pause();
 
-                printAddons.printS("\nIn Mistborn, 1000 years ago the world was conquered by an immortal leader called the Lord Ruler. The people he now ruled under were split into two classes based off whether they supported him in his conquering: The Skaa, or slaves, which did not; and the Nobility, who did.");
-                printAddons.printS("\nEverything else you need to know should be explained in-game as it happens");
-                printAddons.wait(s);
+                pAdd.printS("\nIn Mistborn, 1000 years ago the world was conquered by an immortal leader called the Lord Ruler. The people he now ruled under were split into two classes based off whether they supported him in his conquering: The Skaa, or slaves, which did not; and the Nobility, who did.");
+                pAdd.printS("\nEverything else you need to know should be explained in-game as it happens");
+                pAdd.wait(s);
             }   
         }
-        else printAddons.printS("\nGood, because there are spoilers in this game.");
-        printAddons.pause();
+        else pAdd.printS("\nGood, because there are spoilers in this game.");
+        pAdd.pause();
         return returnValue;
     }
     public static void beginStory(Scanner s){
-        printAddons.printS("\n\n\nYou are a skaa assigned to work the fields in the Tresting Plantation.");
-        printAddons.pause();
-        printAddons.printS("\nA strange man in a strange cloak showed up yesterday who called himself Kelsier, the survivor of Hathsin. And this morning the manor is burning.");
-        printAddons.pause();
-        printAddons.printS("\nThe Survivor of Hathsin made sure that you had no choice but to flee; when the inquisitors find the burned manor and dead nobility, they will assume the skaa were responsible.");
-        printAddons.pause();
-        printAddons.printS("\nYou choose not to go which the other skaa in hiding, but rather to set out on your own in search of a better life.");
-        printAddons.wait(s);
+        pAdd.printS("\n\n\nYou are a skaa assigned to work the fields in the Tresting Plantation.");
+        pAdd.pause();
+        pAdd.printS("\nA strange man in a strange cloak showed up yesterday who called himself Kelsier, the survivor of Hathsin. And this morning the manor is burning.");
+        pAdd.pause();
+        pAdd.printS("\nThe Survivor of Hathsin made sure that you had no choice but to flee; when the inquisitors find the burned manor and dead nobility, they will assume the skaa were responsible.");
+        pAdd.pause();
+        pAdd.printS("\nYou choose not to go which the other skaa in hiding, but rather to set out on your own in search of a better life.");
+        pAdd.wait(s);
     }
     public static player gameTurn(player player, Scanner s){
         player.time.addDay(1);
-        printAddons.printS("\n"+player.time.printDate());
+        pAdd.printS("\n"+player.time.printDate());
         //first: is there any immediate time based events that are happening right now? if so do that
         //second: is there any time and location based "Special" events happening right now? if so do that
         //third: 'regular turn'
@@ -105,7 +113,7 @@ class textGame {
         
         player.phase = player.time.year - 1;
         if(player.time.getYear() < 3) {
-            printAddons.wait(s);
+            pAdd.wait(s);
             player = gameTurn(player, s);
         }
         return player;
@@ -168,7 +176,7 @@ class textGame {
                 // it is travelable to
                 locations.add(a.name());
             }else{
-                printAddons.printS("You are currently at " + areas.values()[i] + "\n");
+                pAdd.printS("You are currently at " + areas.values()[i] + "\n");
                 playerLocationInAreas = i;
             }
         }
@@ -178,7 +186,7 @@ class textGame {
         int optionPicked = options(s,"Travel Locations: ", locationsArray);
         // so if the optionsPicked number is greater then or equal to the player.location in areas.values()[], add 1
         if(optionPicked >= playerLocationInAreas) optionPicked++;
-        printAddons.printS("Your input likely corrosponds to this location: " + areas.values()[optionPicked] + "\n");
+        pAdd.printS("Your input likely corrosponds to this location: " + areas.values()[optionPicked] + "\n");
 
         if(areas.values()[optionPicked].name().toLowerCase().equals("hathsin")){
             boolean willing = option(s,"Are you sure? No one has ever come back from the pits of hathsin","Yes","No");
@@ -186,11 +194,11 @@ class textGame {
                 player.location = areas.values()[optionPicked];
                 player.time.addDay(6);
 
-                printAddons.printS("\nTraveling takes 1 week.");
-                printAddons.printS(travelDescriptions.desc_from_area(player.location));
+                pAdd.printS("\nTraveling takes 1 week.");
+                pAdd.printS(travelDescriptions.desc_from_area(player.location));
             }
             else{
-                printAddons.printS("As you debate this, you realize that your day is spent.");
+                pAdd.printS("As you debate this, you realize that your day is spent.");
             }
         }
         else{
@@ -199,43 +207,49 @@ class textGame {
                 player.location = areas.values()[optionPicked];
                 player.time.addDay(6);
 
-                printAddons.printS("\nTraveling takes 1 week.");
-                printAddons.printS(travelDescriptions.desc_from_area(player.location));
+                pAdd.printS("\nTraveling takes 1 week.");
+                pAdd.printS(travelDescriptions.desc_from_area(player.location));
             }
             else{
-                printAddons.printS("As you debate this, you realize that your day is spent.");
+                pAdd.printS("As you debate this, you realize that your day is spent.");
             }
         }
         return player;
     }
     public static int sleep(int health){
         // So the player sleeps, a day passes, and you gain health.
-        printAddons.printS("You sleep, regaining some health. Health is now 100\n");
-        printAddons.pause();
+        pAdd.printS("You sleep, regaining some health. Health is now 100\n");
+        pAdd.pause();
         return health = 100; // change this so that it can dynamically change?
     }
     public static player explore(player player, Scanner s){
         randomAreaEvents exploreEvent = player.pickEvent(); // eventType, description, enemies
         if(exploreEvent.eventType == eventTypes.ENEMY) player = battle(player,exploreEvent,s);
+        else if(exploreEvent.eventType == eventTypes.INFO) player = InfoEvent(player,exploreEvent,s);
         return player;
     }
     public static void status(player player){
-        printAddons.printS("\nYou are at " + player.location.name());
-        printAddons.pause();
-        printAddons.printS("\nToday is: " + player.time.printDate());
-        printAddons.pause();
-        printAddons.printS("Your health is: \n" + player.health + "\n");
-        printAddons.pause();
-        printAddons.printS("Your munnies you gots in yo purse: " + player.currency + "\n");
+        pAdd.printS("\nYou are at " + player.location.name());
+        pAdd.pause();
+        pAdd.printS("\nToday is: " + player.time.printDate());
+        pAdd.pause();
+        pAdd.printS("Your health is: \n" + player.health + "\n");
+        pAdd.pause();
+        pAdd.printS("Your munnies you gots in yo purse: " + player.currency + "\n");
     }
     public static player battle(player player, randomAreaEvents battle, Scanner s){
-        printAddons.printS("\n"+battle.description);
-        printAddons.printS("\n(There will be a proper battle system in the future)\n");
-        printAddons.pause();
-        printAddons.printS("You Fight " + battle.enemyCount + " " + battle.enemy.name() + " And lose " + battle.healthOfEnemy(battle.enemy)*battle.enemyCount + " Health.");
+        pAdd.printS("\n"+battle.description);
+        pAdd.printS("\n(There will be a proper battle system in the future)\n");
+        pAdd.pause();
+        pAdd.printS("You Fight " + battle.enemyCount + " " + battle.enemy.name() + " And lose " + battle.healthOfEnemy(battle.enemy)*battle.enemyCount + " Health.");
         player.health -= battle.healthOfEnemy(battle.enemy)*battle.enemyCount;
-        printAddons.printS("\nYou gain "+battle.currencyHeldOnEnemy(battle.enemy)*battle.enemyCount+" Boxings (currency)");
+        pAdd.printS("\nYou gain "+battle.currencyHeldOnEnemy(battle.enemy)*battle.enemyCount+" Boxings (currency)");
         player.currency += battle.currencyHeldOnEnemy(battle.enemy)*battle.enemyCount;
+        return player;
+    }
+    public static player InfoEvent(player player, randomAreaEvents info, Scanner s){
+        pAdd.printS("Info Event:");
+        pAdd.printS(info.description);
         return player;
     }
     public static String getInput(Scanner s){
@@ -250,7 +264,7 @@ class textGame {
         return input;
     }
     public static boolean option(Scanner s, String prompt, String expectedValue, String negatoryValue){ // SINGLE OPTION for true / false type questions
-        printAddons.printS(String.format("%s (%s, %s)\n",prompt,expectedValue,negatoryValue));
+        pAdd.printS(String.format("%s (%s, %s)\n",prompt,expectedValue,negatoryValue));
         // clean the text for comparisons
         expectedValue = cleanText(expectedValue, false);
         negatoryValue = cleanText(negatoryValue, false);
@@ -273,9 +287,9 @@ class textGame {
     }
 
     public static int options(Scanner s, String prompt, String[] values){ // MULTIPLE OPTION 
-        printAddons.printS(prompt + " (");
-        for(int i = 0; i < values.length-1; i++) printAddons.printS(String.format("%s, ", (values[i].toString())));
-        printAddons.printS(String.format("%s)\n",values[values.length-1].toString()));
+        pAdd.printS(prompt + " (");
+        for(int i = 0; i < values.length-1; i++) pAdd.printS(String.format("%s, ", (values[i].toString())));
+        pAdd.printS(String.format("%s)\n",values[values.length-1].toString()));
         // clean the text for comparisons
         for(int z = 0; z < values.length; z++) values[z] = cleanText(values[z],true);
         
